a4715e5ae685ee41528dc69d206b0d3b087d34cf
cesine <cesine@yahoo.com>
2022-10-22 12:18:09 -0400

    Type error: Type 'Element' is not assignable to type 'NextPage<{}, {}>'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import type { NextPage } from 'next';
+// import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-function Home(): NextPage {
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
