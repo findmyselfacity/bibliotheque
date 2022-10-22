c840fc6ba5cdda991d94e4d094c879c4c89c4ef7
findmyselfacity <101535569+findmyselfacity@users.noreply.github.com>
2022-10-22 18:59:56 -0400

    chore: changed column order to title 1st

--- components/table/DeweyTable.tsx
+++ components/table/DeweyTable.tsx
@@ -11,16 +11,16 @@ function DeweyTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell>Title</TableCell>
             <TableCell>Dewey Decimal #</TableCell>
-            <TableCell>BookTitle</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {bookList
           .sort((a: BookInfo, b: BookInfo) => (a.title.localeCompare(b.title)))
           .map((book, index) => <TableRow key={index}>
-            <TableCell>{book.deweyDecimal}</TableCell>
             <TableCell>{book.title}</TableCell>
+            <TableCell>{book.deweyDecimal}</TableCell>
           </TableRow>)}
         </TableBody>
       </Table>
